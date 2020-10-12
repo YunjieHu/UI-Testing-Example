18ed7a49e21ba35f6be7a3ee27cba18d0c834771
YunjieHu <yh1112@nyu.edu>
2020-10-12 12:09:05 -0400

    First Commit

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} >
+      <Text>Open up App.js to start testing your app!</Text>
+      <TouchableOpacity accessibilityLabel='testview'  style={{width:100,height:100,backgroundColor:'purple'}}  onPress={() => alert('Hello World')}>
+        <Text>Click Me</Text>
+      </TouchableOpacity>
     </View>
   );
 }
